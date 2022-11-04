c89f7c30c01aea5aa515d94222c0e6a853b00f7a
Horwell <horwellmv@gmail.com>
2022-11-04 01:03:43 -0300

    Agrega listas y metodos para persistencia de clientes y proveedores

--- PA2A/Cliente.py
+++ PA2A/Cliente.py
@@ -1,26 +1,39 @@
 from Persona import Persona
 
 class Cliente(Persona):
-    def __init__(self, dni, esSocio, descuento=0) :
-        super().__init__()
+    def __init__(self, dni, nombre, esSocio=False, descuento=0,telefono="Null",direccion="Null") :
+        super().__init__(telefono,direccion)
         self._dni=dni
+        self._noombre=nombre
         self._esSocio=esSocio
         self._descuento=descuento
+        self.guardar()
     
 # ------------------------------ Metodos de clase
 
-    def __init__(self):
-        super().__init__() + " Dni: {} - Desc: {}%. ".format(
+    def __str__(self):
+        return "Dni: {} - Nombre: {} - Desc: {}%.  - Telefono: {} - Direccion: {}.".format(
             self.dni,
-            self.descuento
+            self.nombre,
+            self.descuento,
+            self.telefono,
+            self.direccion
         )
+    
+    def guardar(self):
+        clienteNew=[self.dni,self.nombre,self.esSocio,self.descuento,self.telefono,self.direccion]
+        clienteList.append(clienteNew)
 
 # ---------------------------- Getters and Setters
 
     @property
-    def id(self):
-        return self._id
+    def dni(self):
+        return self._dni
     
+    @property
+    def nombre(self):
+        return self._noombre
+
     @property
     def esSocio(self):
         return self._esSocio
@@ -37,3 +50,16 @@ class Cliente(Persona):
     def descuento(self, nuevoDescuento):
         self._descuento=nuevoDescuento
 
+# ------------------------------------------ Recursos de la clase
+
+clienteList=[]
+
+def editarCliente(dni, nombre, esSocio=False, descuento=0,telefono="Null",direccion="Null"):
+        list=[dni, nombre, esSocio, descuento,telefono,direccion]
+        for cliente in clienteList:
+            if cliente[0]==list[0]:
+                cliente[1]=list[1]
+                cliente[2]=list[2]
+                cliente[3]=list[3]
+                cliente[4]=list[4]
+                print("--cliente editado--\n")
--- PA2A/Proveedor.py
+++ PA2A/Proveedor.py
@@ -1,21 +1,28 @@
 from Persona import Persona
 
 class Proveedor(Persona):
-    def __init__(self, cuit, razonSocial, categoria):
-        super().__init__()
+    def __init__(self,cuit,razonSocial,categoria,telefono="Null",direccion="Null"):
+        super().__init__(telefono,direccion)
         self._cuit=cuit
         self._razonSocial=razonSocial
         self._categoria=categoria
+        self.guardar()
     
 
 # ----------------------------- Metodos de la clase
 
-    def __init__(self):
-        super().__init__() + " Razon Social: {} - Cuit: {} - Categoria: {}.".format(
-            self.razonSocial,
+    def __str__(self):
+        return "Cuit: {} - Razon Social: {} - Categoria: {} - telefono: {} - Direccion: {}.".format(
             self.cuit,
-            self.categoria
+            self.razonSocial,
+            self.categoria,
+            self.telefono,
+            self.direccion
         )
+    
+    def guardar(self):
+        proovedorNew=[self.cuit,self.razonSocial,self.categoria,self.telefono,self.direccion]
+        proveedorsList.append(proovedorNew)
 
 # ----------------------------- Getters & setters
 
@@ -34,3 +41,17 @@ class Proveedor(Persona):
     @categoria.setter
     def categoria(self,nuevaCategoria):
         self._categoria=nuevaCategoria
+
+# ----------------------------------------- Recursos de la clase
+
+proveedorsList=[]
+
+def editarProveedor(cuit,razonSocial,categoria,telefono="Null",direccion="Null"):
+        list=[cuit,razonSocial,categoria,telefono,direccion]
+        for proveedor in proveedorsList:
+            if proveedor[0]==list[0]:
+                proveedor[1]=list[1]
+                proveedor[2]=list[2]
+                proveedor[3]=list[3]
+                proveedor[4]=list[4]
+                print("--Proveedor editado--\n")
