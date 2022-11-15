a91a1d31794a9c442f848e95e6d808d29b400123
Horwell <horwellmv@gmail.com>
2022-11-15 03:13:18 -0300

    Se crea clase Detalle factura

--- PA2A/DetalleFactura.py
+++ PA2A/DetalleFactura.py
@@ -1,7 +1,62 @@
 from Producto import Producto
 
-class Detalle:
-    def __init__(self,producto:Producto,cantidad,total):
-        self._producto=producto
+from Proveedor import productosList
+
+DetalleList=[]
+
+def newDetalle(nTicket,produc,cant=1):
+    for i in productosList:
+        if i.producto == produc:            
+            if i.stock>=cant:
+                i.stock-=cant
+                produc=i
+                Detalle(nTicket,produc,cant)
+            else: print("Metodo NewDetalle: No hay stock suficiente.") 
+
+
+class Detalle:        
+    def __init__(self,ticketDetalle,articulo:Producto,cantidad=1):
+        self._ticketDetalle=ticketDetalle
+        self._articulo=articulo.producto
         self._cantidad=cantidad
-        self._total=total
+        self._precioU=round(articulo.precio*1.15)  # Agregamos el 15% de valor ganado
+        self._total=round(self.precioU*self.cantidad) # Calculamos el total del detalle
+        print("DetalleClass: Detalle creado")
+        self.guardarDetalle()
+
+    def __str__(self) -> str:
+        return "- {} - {} - {} - {} - {} ".format(
+            self.ticketDetalle,
+            self.articulo,
+            self.cantidad,
+            self.precioU,
+            self.total
+            )
+
+    def guardarDetalle(self):
+        DetalleList.append(self)
+
+    # ----------------------------  Getters & Setters
+    @property
+    def articulo(self):
+        return self._articulo
+    
+    @property
+    def ticketDetalle(self):
+        return self._ticketDetalle
+
+    @property
+    def precioU(self):
+        return self._precioU
+
+    @property
+    def cantidad(self):
+        return self._cantidad
+    
+    @cantidad.setter
+    def cantidad(self,newCantidad):
+        self._cantidad=newCantidad
+    
+    @property
+    def total(self):
+        return self._total
