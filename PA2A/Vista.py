3165fe599d8e9946f78ae44defa5b87aed6d65cd
Horwell <horwellmv@gmail.com>
2022-11-16 15:08:48 -0300

    Incorpora botones, labels y entrys Ventana Ventas

--- PA2A/Vista.py
+++ PA2A/Vista.py
@@ -4,32 +4,15 @@ from tkinter import ttk
 # -------------------------
 from Cliente import *
 from Proveedor import *
+from DetalleFactura import *
+from Factura import *
+
 
 def limpiar(tabla):
     limpiar=tabla.get_children()
     for i in limpiar:
         tabla.delete(i)
-
-def newCliente():
-    Cliente(dni.get(),nombre.get(),descuento.get(),telefono.get(),direccion.get())
-    print("TK: creado Cliente ",nombre.get())
-    for i in clienteList:
-        print(i)
-
-def traerClientes():
-    limpiar(treeC)
-    for i in clienteList:
-        treeC.insert('', 'end', text=i.dni, values=(i.nombre,i.descuento,i.telefono,i.direccion))
         
-#cuit,razonSocial,categoria,telefono="Null",direccion="Null"
-def newProveedor():
-    p=Proveedor(cuit.get(),rSocial.get(),categoria.get(),telefono.get(),direccion.get())
-    print("TK: Creado Proveedor ",rSocial.get())
-
-def traerProveedores():
-    limpiar(treeP)
-    for i in proveedorsList:
-        treeP.insert('', 'end', text=i.cuit, values=(i.razonSocial,i.categoria,i.telefono,i.direccion))
 
 # Consumo= producto, stock: int, categoria, precio: int, vencimiento
 # Limpieza= producto, stock: int, categoria, precio: int, lote
@@ -104,12 +87,95 @@ treePr.grid(row=4,column=7)
 
 # ---------------------------------------------------------------------------------------------------------------- V E N T A S
 
+cli1=Cliente(1,"Horwell",10)
+cli2=Cliente(2,"Gigi",5)
+
+
+pr1=ProductoConsumo("coca",10,"PC",100,"marzo")
+pr2=ProductoConsumo("coco",10,"PC",200,"marzo")
+pr3=ProductoConsumo("cacao",10,"PC",500,"marzo")
+pr4=ProductoLimpieza("trapo",10,"PL",10,"lote1")
+
+
+def detalleVenta():
+    for i in productosList:
+        if i.producto==articulo.get():
+            newDetalle(i.producto,cantidad.get())
+            print("TK: Metodo NewDetalle ejecutado")
+    traerDetalles()
+
+def newFactura():
+    if len(DetalleList)==0:
+        print("TK: Metodo newfactura: No hay detalles a facturar")
+    else:
+        x=Factura(ticket.get(),dniConsumidor.get())
+        total.set(x)
+        print("TK: MEtodo newFactura: Realizado con exito")
+        print(x)
+        newTicket=int(ticket.get())
+        newTicket=newTicket+1
+        ticket.set(newTicket)
+
+def traerDetalles(): #self.articulo,.cantidad, .precioU,.total
+    limpiar(treeV)
+    for i in DetalleList:
+        treeV.insert('', 'end', text=i.cantidad, values=(i.articulo,i.precioU,i.total))
+
 #ELEMENTOS PESTAÑA VENTAS
+ticket =IntVar()
+ticket.set(900000001)
+dniConsumidor = IntVar()
+articulo = StringVar()
+cantidad= IntVar()
+total= StringVar()
 
 
+TicketL=Label(p2,text="Ticket N°:").grid(row=0,column=0, pady=15)
+TicketV=Label(p2, textvariable=ticket).grid(row=0,column=1, pady=15)
+
+telefonoL=Label(p2,text="Cliente DNI:").grid(row=0,column=5)
+telefonoE=Entry(p2, textvariable=dniConsumidor).grid(row=0,column=6)
+
+ArticuloV=Label(p2,text="Articulo:").grid(row=1,column=0)
+ArticuloE=Entry(p2, textvariable=articulo).grid(row=1,column=1)
+
+descuentoL=Label(p2,text="Cantidad:").grid(row=1,column=5)
+descuentoE=Entry(p2, textvariable=cantidad).grid(row=1,column=6)
+
+TotalL=Label(p2,text="TOTAL: $ ").grid(row=7,column=6)
+TotalL2=Label(p2, textvariable=total).grid(row=7,column=7)
+
+# BOTONES VENTAS
+bt1=Button(p2, text='AGREGAR',bg='light blue',command=detalleVenta).grid(row=2,column=6)
+bt2=Button(p2, text='FACTURAR',bg='light blue',command=newFactura).grid(row=10,column=6)
+
+# TABLA VENTAS
+treeV=ttk.Treeview(p2,columns=("producto","precioU","precioT"))
+treeV.column("#0",width=70)
+treeV.column("producto",width=180,anchor=CENTER)
+treeV.column("precioU",width=100,anchor=CENTER)
+treeV.column("precioT",width=100,anchor=CENTER)
+
+treeV.heading("#0",text="UNIDADES")
+treeV.heading("producto",text="PRODUCTO")
+treeV.heading("precioU",text="$ PRECIO Un")
+treeV.heading("precioT",text="$ PRECIO Tot")
+
+treeV.grid(row=4,column=7)
 
 
 # ---------------------------------------------------------------------------------------------------------------- C L I E N T E S
+def newCliente():
+    Cliente(dni.get(),nombre.get(),descuento.get(),telefono.get(),direccion.get())
+    print("TK: creado Cliente ",nombre.get())
+    for i in clienteList:
+        print(i)
+
+def traerClientes():
+    limpiar(treeC)
+    for i in clienteList:
+        treeC.insert('', 'end', text=i.dni, values=(i.nombre,i.descuento,i.telefono,i.direccion))
+
 # VARIABLES PARA INSTANCIAR CLIENTE
 #dni:int, nombre, descuento:int=0,telefono="Null",direccion="Null"
 
@@ -157,14 +223,25 @@ treeC.heading("direccion",text="DIRECCION")
 treeC.grid(row=4,column=7)
 
 # --------------------------------------------------------------------------------------------------------- P R O V E E D O R E S
-# VARIABLES PARA INSTANCIAR PROVEEDOR
 
+#cuit,razonSocial,categoria,telefono="Null",direccion="Null"
+def newProveedor():
+    p=Proveedor(cuit.get(),rSocial.get(),categoria.get(),telefonoP.get(),direccionP.get())
+    print("TK: Creado Proveedor ",rSocial.get())
+
+def traerProveedores():
+    limpiar(treeP)
+    for i in proveedorsList:
+        treeP.insert('', 'end', text=i.cuit, values=(i.razonSocial,i.categoria,i.telefono,i.direccion))
+
+
+# VARIABLES PARA INSTANCIAR PROVEEDOR
 #cuit,razonSocial,categoria,telefono="Null",direccion="Null"
 cuit = IntVar()
 rSocial = StringVar()
 categoria = StringVar()
-telefono = StringVar()
-direccion = StringVar()
+telefonoP = StringVar()
+direccionP = StringVar()
 
 # ELEMENTOS PESTAÑA PROVEEDORES
 #cuit,razonSocial,categoria,telefono="Null",direccion="Null"
@@ -180,10 +257,10 @@ categoriaL=Label(p4,text="Categoria:").grid(row=2,column=0)
 categoriaE=Entry(p4, textvariable=categoria).grid(row=2,column=1)
 
 telefonoL=Label(p4,text="Telefono:").grid(row=0,column=5)
-telefonoE=Entry(p4, textvariable=telefono).grid(row=0,column=6)
+telefonoE=Entry(p4, textvariable=telefonoP).grid(row=0,column=6)
 
 direccionL=Label(p4,text="Direccion:").grid(row=1,column=5)
-direccionE=Entry(p4, textvariable=direccion).grid(row=1,column=6)
+direccionE=Entry(p4, textvariable=direccionP).grid(row=1,column=6)
 
 # Botones Proveedores
 bt1=Button(p4, text='AGREGAR',bg='light blue',command=newProveedor).grid(row=2,column=6)
